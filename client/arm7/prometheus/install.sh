b33ad59132f58f9794a064bc3c3e98cdc4372a21
Francisco Galarza <franciscogalarza@MBP-15-2015.asus-router>
2020-04-05 13:29:09 +1000

    Fixed LaunchAgent service for MacOS

--- client/arm7/prometheus/install.sh
+++ client/arm7/prometheus/install.sh
@@ -5,9 +5,9 @@ curl -SL https://github.com/prometheus/node_exporter/releases/download/v1.0.0-rc
 sudo tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
 
 # Copy service
-sudo cat nodeexporter.service > /etc/systemd/system/nodeexporter.service
+sudo cat node_exporter.service > /etc/systemd/system/node_exporter.service
 
 # Start service
 sudo systemctl daemon-reload \
- && sudo systemctl enable nodeexporter \
- && sudo systemctl start nodeexporter
+ && sudo systemctl enable node_exporter \
+ && sudo systemctl start node_exporter
--- client/macos/prometheus/install.sh
+++ client/macos/prometheus/install.sh
@@ -3,9 +3,10 @@
 # Install
 curl -SL https://github.com/prometheus/node_exporter/releases/download/v1.0.0-rc.0/node_exporter-1.0.0-rc.0.darwin-amd64.tar.gz > node_exporter.tar.gz && \
 sudo tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
+rm node_exporter.tar.gz
 
 # Copy service
-sudo cp nodeexporter.plist /Library/LaunchAgents/nodeexporter.plist
+sudo cp local.node_exporter.plist /Library/LaunchAgents/local.node_exporter.plist
 
 # Start service
-sudo launchctl load /Library/LaunchAgents/nodeexporter.plist
+sudo launchctl load /Library/LaunchAgents/local.node_exporter.plist
