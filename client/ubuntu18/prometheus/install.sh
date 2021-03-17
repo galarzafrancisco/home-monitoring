5892d28e82f9d93ee5304a49e8b8e934e9bcc9ee
Francisco Galarza <franciscogalarza@gmail.com>
2021-03-17 11:57:56 +1100

    Added Ubuntu script

---
+++ client/ubuntu18/prometheus/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+version=node_exporter-1.0.0-rc.0.linux-amd64
+
+# Install
+curl -SL https://github.com/prometheus/node_exporter/releases/download/v1.0.0-rc.0/$version.tar.gz > node_exporter.tar.gz && \
+sudo tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
+sudo rm node_exporter.tar.gz
+
+# Copy service
+sudo cp node_exporter.service /etc/systemd/system/node_exporter.service
+
+# Start service
+sudo systemctl daemon-reload \
+ && sudo systemctl enable node_exporter \
+ && sudo systemctl start node_exporter
